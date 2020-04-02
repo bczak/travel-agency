a65540ae7590908aeed2b08e116214fae163a3a5
algiss00 <algiss00@mail.ru>
2020-04-02 20:02:18 +0100

    little fix

--- ParserApplication/src/main/java/RSP/model/Role.java
+++ ParserApplication/src/main/java/RSP/model/Role.java
@@ -2,6 +2,5 @@ package RSP.model;
 
 public enum Role {
     ROLE_ADMIN,
-    ROLE_USER,
-    ROLE_ABAS
+    ROLE_USER
 }
--- ParserApplication/src/main/java/RSP/security/SecurityConstants.java
+++ ParserApplication/src/main/java/RSP/security/SecurityConstants.java
@@ -29,7 +29,6 @@ public class SecurityConstants
 
     public final static String USER = "ROLE_USER";
     public final static String ADMIN = "ROLE_ADMIN";
-    public final static String ABAS = "ROLE_ABAS";
 }
 
 
