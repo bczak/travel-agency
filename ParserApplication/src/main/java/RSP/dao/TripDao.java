1469c52a77fbcc402ea583dd7ecb4c2fb689094a
Karel Poledna <poledkar@fel.cvut.cz>
2020-04-03 15:10:22 +0200

    TripDao.getByName improved

--- ParserApplication/src/main/java/RSP/dao/TripDao.java
+++ ParserApplication/src/main/java/RSP/dao/TripDao.java
@@ -4,8 +4,6 @@ import RSP.model.Trip;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Objects;
 
@@ -44,13 +42,10 @@ public class TripDao extends AbstractDao<Trip> {
     }
 
     public Trip getByName(String name) {
-        TypedQuery<Trip> query = em.createNamedQuery("Trip.getByName", Trip.class);
-        query.setParameter("Name", name);
-        try {
-            Trip t = query.getSingleResult();
-            return t;
-        } catch (NoResultException ex) {
-            return null;
-        }
+        return em.createNamedQuery("Trip.getByName", Trip.class)
+                .setParameter("Name", name)
+                .setMaxResults(1)
+                .getResultList()
+                .stream().findFirst().orElse(null);
     }
 }
