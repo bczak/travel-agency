f244b07d0c35aa0745dd545fe6d07fd11baf19cc
Jakhongir <tashpjak@fel.cvut.cz>
2020-04-12 01:34:41 +0200

    Testing

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ const api_url = "http://localhost:8080";
 
 export async function getTripsApi(by, order) {
   try {
-    const response = await fetch(`${api_url}/trips/sort?by=${by}&order=${order}`);
+    const response = await fetch(`${api_url}/api/trips/sort?by=${by}&order=${order}`);
     const data = await response.json();
     return data;
   } catch (error) {
