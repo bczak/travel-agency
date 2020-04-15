69aa5c705040cef4eb198db8eddf18a1531269a6
Jakhongir <tashpjak@fel.cvut.cz>
2020-04-15 14:57:36 +0200

    Parser working

--- python-parser/process.py
+++ python-parser/process.py
@@ -3,6 +3,13 @@ import sys
 import requests
 import config
 import nltk
+import string
+import re
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('punkt')
 
 filename = sys.argv[1]
 file = open('json/' + filename + '.json', 'r')
@@ -13,7 +20,34 @@ parsed_json = (json.loads(json_data))
 words = set()
 
 text = parsed_json['description']
+text = re.sub(r'\d+', '', text)
+
+translate_table = dict((ord(char), None) for char in string.punctuation)
+text = text.translate(translate_table)
+
+text = text.lower()
+
+tokens = nltk.word_tokenize(text)
+
+lem = nltk.WordNetLemmatizer()
+tokens = [lem.lemmatize(token) for token in tokens]
+
+stop = stopwords.words('english')
+tokens = [token for token in tokens if token not in stop]
+
+syns = []
+
+for word in tokens:
+    for syn in nltk.corpus.wordnet.synsets(word):
+        for lemma in syn.lemmas():
+            w = lemma.name()
+            if w not in tokens or w not in syns:
+                syns.append(lemma.name())
+
+tokens = tokens + syns
 
 for rule in config.rules:
-    tokens = nltk.word_tokenize(text)
-    print(tokens)
+    index = 0
+    for word in config.rules[rule]:
+        index += tokens.count(word)
+    print(rule + ' : ' + str(index))
