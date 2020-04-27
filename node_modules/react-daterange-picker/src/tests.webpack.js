7aed5f9e8124136e1c0ca8585d11b3507ba71d89
Jakhongir <tashpjak@fel.cvut.cz>
2020-04-27 22:06:04 +0200

    Refactoring

--- node_modules/react-daterange-picker/src/tests.webpack.js
+++
@@ -1,3 +0,0 @@
-import 'babel-polyfill';
-var context = require.context('../src', true, /\.spec\.js$/);
-context.keys().forEach(context);
--- node_modules/react-daterange-picker/src/utils/isMomentRange.js
+++
@@ -1,5 +0,0 @@
-import moment from '../moment-range';
-
-export default function isMomentRange(val) {
-  return val && val.start && val.end && moment.isMoment(val.start) && moment.isMoment(val.end);
-}
