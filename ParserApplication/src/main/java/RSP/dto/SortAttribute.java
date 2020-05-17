e2bfe35fb4110294ae2f5d0ab9dcdfa4ee6a4f1b
Karel Poledna <poledkar@fel.cvut.cz>
2020-05-17 23:07:52 +0200

    Secondary ordering by name to ensure total order

--- ParserApplication/src/main/java/RSP/dto/SortAttribute.java
+++ ParserApplication/src/main/java/RSP/dto/SortAttribute.java
@@ -1,19 +1,25 @@
 package RSP.dto;
 
 public enum SortAttribute {
-    ID("id"),
-    NAME("name"),
-    START("startDate"),
-    LENGTH("length"),
-    PRICE("price");
+    ID("id", true),
+    NAME("name", true),
+    START("startDate", false),
+    LENGTH("length", false),
+    PRICE("price", false);
 
     private final String columnName;
+    private final boolean total;
 
-    SortAttribute(String columnName) {
+    SortAttribute(String columnName, boolean total) {
         this.columnName = columnName;
+        this.total = total;
     }
 
     public String getColumnName() {
         return columnName;
+    }
+
+    public boolean isTotalOrdering() {
+        return total;
     }
 }
